c1247103c5b14ea83b067c4917a28d77e05a220d
Gabriel Ferreiro <gbril9119@gmail.com>
2020-02-05 20:39:11 +0100

    Fixed scrolling with new components

--- src/Tab.tsx
+++ src/Tab.tsx
@@ -22,7 +22,7 @@ class TabButtonsContainer extends React.Component<TabButtonsContainerProps> {
 	private ref = React.createRef<HTMLDivElement>();
 
 	scrollToThis = () => {
-		this.ref && this.ref.current && this.ref.current.scrollIntoView({ block: 'end', behavior: 'smooth' });
+		this.ref && this.ref.current && this.ref.current.scrollIntoView({ block: 'start', behavior: 'smooth' });
 	}
 
 	render() {
@@ -52,31 +52,6 @@ class TabButtonsContainer extends React.Component<TabButtonsContainerProps> {
 		);
 	}
 }
-
-// const TabButtonsContainer: React.FC<TabButtonsContainerProps> = ({ title, type, disableOnType, onTypeSelected }: TabButtonsContainerProps) => {
-// 	const typesRange = Array.from(Array(18).keys());
-
-// 	let buttons = typesRange.map(t => {
-// 		const classes = ['type-button', Types[t].toLocaleLowerCase()];
-
-// 		if (t === type)
-// 			classes.push('selected');
-
-// 		return <button key={t} className={classnames(classes)} disabled={disableOnType !== undefined && disableOnType === t} onClick={() => onTypeSelected && onTypeSelected(t)}>{localizeType(t)}</button>
-// 	});
-
-// 	if (disableOnType !== undefined) {
-// 		const typeClass = `type-button ${type < 0 ? 'selected' : ''}`
-// 		buttons = [<button key={-1} className={typeClass} onClick={() => onTypeSelected && onTypeSelected(-1)}>Ninguno</button>].concat(buttons);
-// 	}
-
-// 	return (
-// 		<div>
-// 			<h5>{title}</h5>
-// 			<div className='buttons-grid'>{buttons}</div>
-// 		</div>
-// 	);
-// }
 
 
 type AttackTabProps = {
@@ -115,11 +90,21 @@ export const DefenseTab: React.FC<DefenseTabProps> = ({ primaryType, secondaryTy
 	const secondaryTypeRef = useRef(null);
 	const resultRef = useRef(null);
 
+	const primaryTypeSelected = (type: Types) => {
+		onPrimaryTypeSelected(type);
+		scrollToRef(secondaryTypeRef);
+	};
+
+	const secondaryTypeSelected = (type: Types) => {
+		onSecondaryTypeSelected(type);
+		scrollToRef(resultRef);
+	};
+
 	return (
 		<>
-			<TabButtonsContainer title={t('PRIMARY_TYPE_LBL')} type={primaryType} onTypeSelected={onPrimaryTypeSelected} />
-			<TabButtonsContainer ref={onSecondaryTypeSelected} title={t('SECONDARY_TYPE_LBL')} type={secondaryType} onTypeSelected={onSecondaryTypeSelected} disableOnType={primaryType} />
-			<Result t={t} primaryType={primaryType} secondaryType={secondaryType} />
+			<TabButtonsContainer title={t('PRIMARY_TYPE_LBL')} type={primaryType} onTypeSelected={primaryTypeSelected} />
+			<TabButtonsContainer ref={secondaryTypeRef} title={t('SECONDARY_TYPE_LBL')} type={secondaryType} onTypeSelected={secondaryTypeSelected} disableOnType={primaryType} />
+			<Result ref={resultRef} t={t} primaryType={primaryType} secondaryType={secondaryType} />
 		</>
 	);
 }
